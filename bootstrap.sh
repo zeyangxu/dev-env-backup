da21c668001db413864c7ba4429df6cb0b3bd844
xuzeyang <xuzeyang@bytedance.com>
2023-09-21 17:03:30 +0800

    bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# git
+brew install git
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -15,4 +21,18 @@ brew install tmux
 
 # replace tmux config file in the system
 cp ./.tmux.conf ~/.tmux.conf
+
+# neovim
+brew install neovim
+
+# lunarvim
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+
+# download nvim config from github
+git clone https://github.com/zeyangxu/nvim-config.git nvim
+cp -rf ./nvim ~/.config/nvim
+
+# download lvim config from github
+git clone https://github.com/zeyangxu/lvim-config.git lvim
+cp -rf ./lvim ~/.config/lvim
 
